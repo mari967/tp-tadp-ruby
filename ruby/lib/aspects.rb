7f861978638c8c087309b463f7ad29a98baccaca
mari967 <mar.y.sol.240@gmail.com>
2021-10-16 01:37:37 -0300

    Condición por tipo de parámetros y cantidad

--- ruby/lib/aspects.rb
+++ ruby/lib/aspects.rb
@@ -1,37 +1,54 @@
 class Aspects
-  attr_accessor
   def initialize
-    @origenes =[]
+    @origenes = []
+  end
+
+  def self.origenes_inexistentes?(origenes)
+    (origenes & ObjectSpace.each_object(Module).to_a).empty?
   end
 
   def self.on(*origenes, &bloque)
     raise ArgumentError.new "wrong number of arguments (0 for +1)" if origenes.empty?
-    raise ArgumentError.new "origen vacío" if origenesInexistentes(origenes)
-    @origenes += origenes
-
+    raise ArgumentError.new "origen vacío" if origenes_inexistentes?(origenes)
+    origenes.map { |origen| AspectForOrigin.new(origen).instance_exec(&bloque) }.flatten(1)
+    #Cuando el resultado era vacío, retornaba [[]]. FLatten es para ese caso. En otro no afecta
   end
 
-  def origenesInexistentes(*origenes)
-       origenes & ObjectSpace.each_object(Module).to_a.empty?
-  end
-
-  def where *condiciones #DEeb existir un where general y uno individual para cada origen,
-    # para que filtren sus propios métodos
-    #retorna el resultado de aplicar condiciones a cada origen
-    # Las condiciones son alias, para llamar al método correspondiente en cada origen
-    # Lista
-  end
 end
 
-module Condicion #cada condicion debe retornar un proc
+module Condicion #cada condicion debe retornar un proc. El proc retorna un booleano
   def name(regex)
     proc do |method|
       regex.match? method
     end
   end
 
-  def has_parameters cantidad, tipo=all, regexNombreParametros=/.*/
+  def has_parameters cantidad, tipo=nil
+    proc do |method_to_filter|
+      cumple = false
+      origen_to_filter = if @origen.is_a? Module
+                           @origen.new
+                         else @origen
+                         end
 
+      lista = origen_to_filter.methods
+      metodo = origen_to_filter.method(method_to_filter)
+      lista_parametros = origen_to_filter.method(method_to_filter).parameters
+      if !tipo.nil?
+        obligatoriedad = case tipo
+                         when 'mandatory'
+                           :req
+                         when 'optional'
+                           :opt
+                         end
+        cumple = lista_parametros.select do |lista_parametro|
+                                          lista_parametro[0] == obligatoriedad end .size == cantidad
+
+      else
+        cumple = lista_parametros.size == cantidad
+      end
+      cumple
+    end
   end
 
   def neg condicion
@@ -47,11 +64,16 @@ class AspectForOrigin
 
   def initialize origen
     @origen = origen
-    @metodos_por_filtrar = origen.methods
+    @metodos_por_filtrar = if @origen.is_a? Module
+                             @origen.instance_methods
+                           else origen.methods
+                           end
   end
 
   def where *condiciones
-    @origen.methods.select { |method| condiciones.all? { |condicion| condicion.call(method)} }
+    #methods en clases y módulos no funciona. Incluye los métodos hasta object
+    # Las clases y los módulos heradan de object por otro camino
+    @metodos_por_filtrar.select { |method| condiciones.all? { |condicion| condicion.call(method) } }
   end
 
 end
--- ruby/spec/condiciones_spec.rb
+++ ruby/spec/condiciones_spec.rb
@@ -1,32 +1,73 @@
+class MiClase
+  def foo
+  end
+
+  def bar
+  end
+end
+
 describe 'Condiciones' do
-  class MiClase
-    def foo
+
+  describe 'Condición Selector' do
+
+    it 'Filtro por método name fo{2} retorna array con un elemento' do
+      resultado = []
+      Aspects.on MiClase do
+        resultado = where(name(/fo{2}/))
+      end
+      expect(resultado).to  eq [:foo]
     end
 
-    def bar
+    it 'Filtro por método name fo{2} y foo retorna array con un elemento' do
+      resultado = []
+      Aspects.on MiClase do
+        resultado = where name(/fo{2}/), name(/foo/)
+      end
+      expect(resultado).to eq ([:foo])
+    end
+
+    it 'Filtro por método name ^fi+ retorna array vacío' do
+      listaResultado = []
+      Aspects.on MiClase do
+        listaResultado = where name(/^fi+/)
+      end
+      expect(listaResultado).to eq([])
+    end
+
+    it 'Filtro por método name ^fi+ retorna array vacío' do
+      listaResultado = Aspects.on MiClase do where name(/foo/), name(/bar/) end
+      expect(listaResultado).to eq([])
     end
   end
 
+  describe 'Condición Cantidad de Parámetros' do
+    class MiClase2
+      def foo(p1, p2, p3, p4='a', p5='b', p6='c')
+      end
+      def bar(p1, p2='a', p3='b', p4='c')
+      end
+    end
 
- it 'Filtro por método name fo{2} retorna array con un elemento' do
-   expect { Aspects.on MiClase do
-       where name(/fo{2}/)
-     end }.should eq([:foo])
- end
 
-  it 'Filtro por método name fo{2} y foo retorna array con un elemento' do
-    expect { Aspects.on MiClase do
-      where name(/fo{2}/), name(/foo/)
-    end }.to match([:foo])
-  end
+    it 'Filtro para tres parámetros obligatorios de MiClase retorna foo' do
+    lista_resultado = Aspects.on MiClase2 do
+      where has_parameters(3, 'mandatory')
+    end
+    expect(lista_resultado).to eq([:foo])
+    end
 
-  it 'Filtro por método name ^fi+ retorna array vacío' do
-    listaResultado = Aspects.on MiClase do where name(/^fi+/) end
-    expect(listaResultado).to be_empty
-  end
+    it 'Filtro para seis parámetros cualesquiera de MiClase retorna foo' do
+      lista_resultado = Aspects.on MiClase2 do
+        where has_parameters(6)
+      end
+      expect(lista_resultado).to eq([:foo])
+    end
 
-  it 'Filtro por método name ^fi+ retorna array vacío' do
-    listaResultado = Aspects.on MiClase do where name(/foo/), name(/bar/) end
-    expect(listaResultado).to be_empty
+    it 'Filtro para tres parámetros opcionales de MiClase retorna foo y bar' do
+      lista_resultado = Aspects.on MiClase2 do
+        where has_parameters(3, 'optional')
+      end
+      expect(lista_resultado).to eq([:foo, :bar])
+    end
   end
 end
